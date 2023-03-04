39c52379fe47f72925ea4c69cc959a91cda435e6
Constantine Cherkashyn <cons1@protonmail.ch>
2023-03-04 16:54:01 -0500

    Test

--- src/main.py
+++ src/main.py
@@ -9,15 +9,48 @@ click.clear()
 
 
 def amplifier_power_required():
-    distance_from_source = click.prompt('Listener distance from source in meters', default=100)
-    desired_level = click.prompt('Desired dB SPL at distance', default=85)
-    sensitivity = click.prompt('Sensitivity rating in dB', default=88)
+    print('Amplifier Power Required')
+    print('')
+    print('This calculator provides the required electrical power '
+          '(power output from the amplifier) to produce a desired Sound Pressure Level (SPL) '
+          'at a given distance, along with an amount of headroom to keep the amplifier(s) out of clip.')
+    print('')
+
+    units = click.prompt('Use meters or feet? Default is', default="meters")
+
+    if units == "meters":
+        distance_from_source = click.prompt('Listener distance from speaker (in meters)', default=3.7)
+        reference_distance = 1
+    elif units == "feet":
+        distance_from_source = click.prompt('Listener distance from speaker (in feet)', default=12.0)
+        reference_distance = 3.281
+    else:
+        print('Incorrect units specified.')
+        return 0
+
+    if distance_from_source <= 0:
+        print('Distance must be greater than 0.')
+        return 0
+
+    desired_level = click.prompt('Desired dB SPL at distance', default=80)
+    sensitivity = click.prompt('Speaker sensitivity rating in dB', default=85)
     headroom = click.prompt('Amplifier headroom in dB', default=3)
-    reference_distance = 1
 
-    power_required = int(10 ** (((desired_level + headroom - sensitivity) + 20 * math.log((distance_from_source / reference_distance), 10)) / 10))
+    power_required = 10 ** (((desired_level + headroom - sensitivity) + 20 * math.log((distance_from_source / reference_distance), 10)) / 10)
 
-    print("Power required: " + str(power_required) + " watts")
+    if power_required < 1:
+        power_required = round(power_required, 4)
+    else:
+        power_required = int(power_required)
+
+    print('')
+    print('Power required: ' + str(power_required) + ' watts')
+    print('')
+    print('What does this mean?')
+    print('Assuming the speaker and amplifier output have the same impedance, '
+          'the amplifier will need to have ' + str(power_required) + ' watts of power to drive the speaker speaker at '
+          + str(desired_level) + 'dB of SPL at ' + str(distance_from_source) + ' ' + str(units) + '. The speaker must also be able to handle this much power.')
+
     return power_required
 
 if __name__ == '__main__':

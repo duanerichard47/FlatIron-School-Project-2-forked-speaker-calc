064ceb85b52bb26c07cccec6a611eb0b672c8829
Constantine Cherkashyn <cons1@protonmail.ch>
2023-03-04 11:41:37 -0500

    First template

--- src/main.py
+++ src/main.py
@@ -1,16 +1,44 @@
 #!/usr/bin/env python
 
 import click
+from pick import pick
 
+click.clear()
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo(f"Hello {name}!")
+
+def nothing():
+    pass
 
 if __name__ == '__main__':
-    hello()
+
+    title = 'Speaker System Design Calculator'
+    
+    options = ['Amplifier Power Required',
+               'Ohm\'s Law / Watt\'s Law',
+               'db Voltage Ratio',
+               'db Power Ratio',
+               'Inverse Square Law',
+               'Transformer Delivered Power',
+               'Speaker Box Enclosure Designer',
+               'Speaker Box Volume Designer',
+               'Sealed or Ported Enclosure',
+               'Driver Displacement',
+               '2-Way Crossover',
+               '3-Way APC Crossover',
+               'Series Notch Filter',
+               'Parallel Notch Filter',
+               'Driver Attenuation Circuit',
+               'Impedance Equalizer',
+               'Contour Network',
+               'Air Core Inductor Designer',
+               'Exit Application']
+    
+    option, index = pick(options, title, "->")
+    
+    print("")
+    print(" " + title)
+    print("")
+    print(option)
+    print(index)
+
 
